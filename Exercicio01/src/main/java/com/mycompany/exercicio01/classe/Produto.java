be86c36f41925fd98370c8a53d59d3f53363f757
Mayra Pereira <Mayra Pereira@Mayra>
2017-08-22 15:55:16 -0300

    Criando classe produto

---
+++ Exercicio01/src/main/java/com/mycompany/exercicio01/classe/Produto.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.exercicio01.classe;
+
+/**
+ *
+ * @author Mayra Pereira
+ */
+public class Produto {
+    private Integer cod_prod;
+    private String nome;
+    private String desc;
+    private float vVenda;
+    private float vCompra;
+    private String categoria;
+
+    public Integer getCod_prod() {
+        return cod_prod;
+    }
+
+    public void setCod_prod(Integer cod_prod) {
+        this.cod_prod = cod_prod;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public float getvVenda() {
+        return vVenda;
+    }
+
+    public void setvVenda(float vVenda) {
+        this.vVenda = vVenda;
+    }
+
+    public float getvCompra() {
+        return vCompra;
+    }
+
+    public void setvCompra(float vCompra) {
+        this.vCompra = vCompra;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+    
+    
+}
